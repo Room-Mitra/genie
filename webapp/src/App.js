bb327ce8f18ff1f87ec67289c716bbcc4f7815a4
Adithya Prabhu <adithyaprabhu@Adithyas-MacBook-Air.local>
2024-12-19 05:50:33 +0530

    webapp added

---
+++ webapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Devices from './Modules/AdminModule/Pages/Devices/index';
+// import Devices from "./Modules/AdminModule/Pages/Devices"
+
+function App() {
+  return (
+    <div className="App">
+      <Devices />
+    </div>
+  );
+}
+
+export default App;
