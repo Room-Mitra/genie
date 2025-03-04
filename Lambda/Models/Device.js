dfe61e4daf955d6f5edaef42e50fe3958939a905
Adithya Prabhu <adithyaprabhu@192.168.1.3>
2025-03-04 17:30:16 +0530

    device class updated in all 3 modules

--- Lambda/Models/Device.js
+++ Lambda/Models/Device.js
@@ -1,9 +1,15 @@
 class Device {
-    constructor(deviceId, roomId, roomInfo = defaultRoomInfo, deviceInfo = defaultDeviceInfo) {
+    constructor(deviceId, roomId) {
         this.deviceId = deviceId;
         this.roomId = roomId;
-        this.roomInfo = roomInfo;
-        this.deviceInfo = deviceInfo;
+        this.propertyName = defaultRoomInfo.propertyName;
+        this.floor = defaultRoomInfo.floor;
+        this.room = defaultRoomInfo.room;
+        this.roomTags = defaultRoomInfo.roomTags;
+        this.roomNotes = defaultRoomInfo.notes;
+        this.deviceType = defaultDeviceInfo.deviceType;
+        this.deviceTags = defaultDeviceInfo.deviceTags;
+        this.deviceNotes = defaultDeviceInfo.notes;
         this.registeredAtUTC = new Date().toISOString();
     }
 }
--- webapp/src/Services/APIService.js
+++ webapp/src/Services/APIService.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import cacheInstance from "./APICache";
 
-export const httpGet = async (url, bypassCache = false) => {
-    if (bypassCache) {
-        const response = await httpGetUncached(url);
+export const httpGet = async (url, bypassBrowserCache = false, bypassServerCache = false) => {
+    if (bypassBrowserCache) {
+        const response = await httpGetUncached(url, bypassServerCache);
         return response.data;
     }
 
     const cachedValue = await cacheInstance.get(url);
     if (!cachedValue) {
-        const response = await httpGetUncached(url);
+        const response = await httpGetUncached(url, bypassServerCache);
         cacheInstance.put(url, response.data); // TODO: handle error in response);
         return response.data;
     }
@@ -17,8 +17,17 @@ export const httpGet = async (url, bypassCache = false) => {
     return cachedValue;
 }
 
-export const httpGetUncached = async (url, bypassCache = false) => {
-    const response = await axios.get(url);
+const httpGetUncached = async (url, bypassServerCache = false) => {
+    if (bypassServerCache) {
+        url += url.includes("?") ? "&bypassCache=true" : "?bypassCache=true";
+    }
+    const response = await axios.get(url);  // TODO: handle error
+    console.log("API RESPONSE :: ", url, response);
+    return response;
+}
+
+export const httpPut = async (url, data) => {
+    const response = await axios.put(url, data);  // TODO: handle error
     console.log("API RESPONSE :: ", url, response);
     return response;
 }
