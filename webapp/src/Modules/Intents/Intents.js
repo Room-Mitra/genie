6859909d8e14a0a08f8f4cafd664d9967b08f477
Adithya Prabhu <adithyaprabhu@Adithyas-MacBook-Air.local>
2025-03-21 17:14:50 +0530

    webapp / intents table / time format

--- webapp/src/Modules/Intents/Intents.js
+++ webapp/src/Modules/Intents/Intents.js
@@ -9,13 +9,30 @@ const INTENTS_API_URI = '/intents';
 const Intents = () => {
 
     const [allIntents, setAllIntents] = useState(null);
+    // Row Data: The data to be displayed.
+    const [rowData, setRowData] = useState(null);
+    // Column Definitions: Defines the columns to be displayed.
+    const [colDefs, setColDefs] = useState([
+        // { headerName: "Requested Time", field: "requestedTime", filter: true, editable: true },
+        { headerName: "Requested Time", valueGetter: p => new Date(p.data.requestedTime).toLocaleTimeString(), filter: true },
+        { headerName: "Intent Name", field: "intentName", filter: true, editable: true },
+        { headerName: "Room ID", field: "roomId", filter: true, editable: false },
+        // { headerName: "Property Name", field: "propertyName", filter: true, editable: true },
+        // { headerName: "Room in Floor", field: "roomFloor", filter: true, editable: true, cellDataType: 'number' },
+        // { headerName: "Room Tags", field: "roomTags", filter: true, editable: true },
+        // { headerName: "Room Notes", field: "roomNotest", filter: true, editable: true },
+        // { headerName: "Device Notes", field: "deviceNotes", filter: true, editable: true },
+        // { headerName: "Device ID", field: "deviceId" },
+        // { headerName: "Device Registered On", valueGetter: p => new Date(p.data.registeredAtUTC).toLocaleString(), filter: "agDateColumnFilter" },
+        // { headerName: " ", cellRenderer: SaveChangesButtonComponent }
+    ]);
 
     useEffect(() => {
         getAllIntentsData()
     }, []);
 
     const getAllIntentsData = async () => {
-        const intents = await httpGet(EC2_API_ENDPOINT + INTENTS_API_URI + "/" + Math.floor(Date.now() / (24 * 60 * 60 * 1000)));
+        const intents = await httpGet(EC2_API_ENDPOINT + INTENTS_API_URI + "/" + Math.floor(Date.now() / (24 * 60 * 60 * 1000)), true);
         console.log("Intents for Today = ", intents)
         setAllIntents(intents);
         setRowData(intents);
@@ -32,37 +49,18 @@ const Intents = () => {
         return <button onClick={(eventObj) => handleClick(rowInfo, eventObj)}>Save Changes</button>;
     };
 
-    // Row Data: The data to be displayed.
-    const [rowData, setRowData] = useState(null);
-
-    // Column Definitions: Defines the columns to be displayed.
-    const [colDefs, setColDefs] = useState([
-        { headerName: "Requested Time", field: "requestedTime", filter: true, editable: true },
-        { headerName: "Room ID", field: "roomId", filter: true, editable: false },
-        { headerName: "Intent Name", field: "intentName", filter: true, editable: true },
-        // { headerName: "Property Name", field: "propertyName", filter: true, editable: true },
-        // { headerName: "Room in Floor", field: "roomFloor", filter: true, editable: true, cellDataType: 'number' },
-        // { headerName: "Room Tags", field: "roomTags", filter: true, editable: true },
-        // { headerName: "Room Notes", field: "roomNotest", filter: true, editable: true },
-        // { headerName: "Device Notes", field: "deviceNotes", filter: true, editable: true },
-        // { headerName: "Device ID", field: "deviceId" },
-        // { headerName: "Device Registered On", valueGetter: p => new Date(p.data.registeredAtUTC).toLocaleString(), filter: "agDateColumnFilter" },
-        // { headerName: " ", cellRenderer: SaveChangesButtonComponent }
-    ]);
-
-    if (!allIntents) {
-        return (<div>Loading Intents...</div>); // TODO :: add loader
-    }
-
     return (
         <div>
             <h1>Intents</h1>
-            <div style={{ height: 500 }}>
-                <DataTable
-                    rowData={rowData}
-                    columnDefs={colDefs}
-                />
-            </div>
+            {allIntents && (
+                <div style={{ height: 500 }}>
+                    <DataTable
+                        rowData={rowData}
+                        columnDefs={colDefs}
+                    />
+                </div>
+            )}
+            {!allIntents && (<div>Loading Intents...</div>)}
         </div>
     );
 }
