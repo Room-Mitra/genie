d87a37a045cb026c64e43148a0ace5a3a57c5dd5
Adithya Prabhu <adithyaprabhu@Adithyas-MacBook-Air.local>
2025-05-22 10:36:43 +0530

    EC2 & webapp :: moved secret to env file, added token in all api calls from ui

--- EC2/routes/Login/Login.controller.js
+++ EC2/routes/Login/Login.controller.js
@@ -2,30 +2,33 @@ const express = require('express');
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 
-const SECRET_KEY = "your_secret_key"; // Replace with a secure key
+const SECRET_KEY = process.env.SECRET_KEY;
 
 // Dummy User Data (Replace with database logic)
 const users = [
-    { username: "admin", password: "password123" },
-    { username: "guest", password: "guest123" },
+    { username: "admin", password: "password123", hotelId: "ROOM GENIE" },
+    { username: "guest", password: "guest123", hotelId: "ROOM GENIE" },
 
 ];
+
+const getUserDetails = (username) => {
+    return users.find((user) => user.username === username);
+}
 
 router.post('/', async (req, res) => {
 
     const { username, password } = req.body;
-    const user = users.find(
-        (user) => user.username === username && user.password === password
-    );
 
-    console.log(user, username, password, req.body)
-    if (!user) {
+    const user = getUserDetails(username);
+    if (!user || user.password !== password) {
         res.status(401).json({ message: "Invalid username or password" });
         return;
     }
 
+    const hotelId = user.hotelId;
+
     // Generate JWT
-    const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: "1h" });
+    const token = jwt.sign({ username, hotelId }, SECRET_KEY, { expiresIn: "10h" });
     res.json({ token });
 
 })
