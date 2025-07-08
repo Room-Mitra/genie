c6fde6ddf9eb9a4999ff018e5e2f85c2a842adcd
Adithya Prabhu <adithyaprabhu@Adithyas-MacBook-Air.local>
2025-07-08 10:24:13 +0530

    Ec2 :: add user api added

--- EC2/routes/Login/Login.repository.js
+++ EC2/routes/Login/Login.repository.js
@@ -10,9 +10,9 @@ const getUser = async (userId) => {
         KeyConditionExpression: "id=:id"
     };
     try {
-        console.info("Accessing Login details from DB with params", params);
+        console.info(`${userId} ->` + "Accessing Login details from DB with params", params);
         const userData = await DDB.query(params).promise();
-        console.info("User Data From DB :: ", userData, " :: for input params ::", params);
+        console.info(`${userId} ->` + "User Data From DB :: ", userData, " :: for input params ::", params);
         if (userData && userData.Items && userData.Items.length) {
             return { ...userData.Items[0] };
         }
