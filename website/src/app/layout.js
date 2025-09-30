c84cd19e569b96b4245e20935fb0ad2bd590505e
Chai Nadig <chaitanya.nadig@gmail.com>
2025-10-01 00:21:38 +0530

    NextJS Website (#50)
    * NextJS Website
    
    * Update favicon

---
+++ website/src/app/layout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata = {
+  title: 'Room Mitra',
+  description: 'In-room voice assistant for hotel rooms',
+  favicon: '/favicon.ico',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
