39eda0dff369a0d3eeb9579a4e4c5f6b4f9a76f6
Chai Nadig <chaitanya.nadig@gmail.com>
2025-09-30 21:06:44 +0530

    Terraform for EC2 (#49)
    * Terraform for EC2
    
    * Update readme
    
    * prettier for terraform

---
+++ terraform/main.tf
@@ -0,0 +1,121 @@
+# -- Identity
+data "aws_caller_identity" "me" {}
+
+# -- Networking (use default VPC to stay free/zero-config)
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# -- AMI: Amazon Linux 2023 (x86_64)
+data "aws_ami" "al2023" {
+  most_recent = true
+  owners      = ["137112412989"] # Amazon
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+# -- Security Group: allow HTTP/HTTPS only
+resource "aws_security_group" "web" {
+  name        = "rm-web-sg"
+  description = "Allow web in; all out"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description      = "HTTP"
+    protocol         = "tcp"
+    from_port        = 80
+    to_port          = 80
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS"
+    protocol         = "tcp"
+    from_port        = 443
+    to_port          = 443
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    description      = "All egress"
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = var.tags
+}
+
+# -- IAM: SSM access (Session Manager, no SSH)
+data "aws_iam_policy_document" "ec2_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ssm_role" {
+  name               = "rm-ec2-ssm-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume.json
+  tags               = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "rm-ec2-ssm-instance-profile"
+  role = aws_iam_role.ssm_role.name
+}
+
+# -- Optional SSH keypair (kept disabled)
+resource "aws_key_pair" "rm_key" {
+  count      = var.create_key_pair ? 1 : 0
+  key_name   = "rm-ec2-key"
+  public_key = var.public_key_openssh
+}
+
+# -- EC2 instance
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.al2023.id
+  instance_type          = var.instance_type
+  subnet_id              = element(data.aws_subnets.default.ids, 0)
+  vpc_security_group_ids = [aws_security_group.web.id]
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+
+  # If you really want SSH, flip create_key_pair=true and uncomment:
+  # key_name = aws_key_pair.rm_key[0].key_name
+
+  user_data                   = templatefile("${path.module}/user_data.sh", {})
+  user_data_replace_on_change = true
+
+
+
+  tags = merge(var.tags, { Name = "roommitra-web" })
+}
+
+# -- Elastic IP (free while attached)
+resource "aws_eip" "web" {
+  instance = aws_instance.web.id
+  domain   = "vpc"
+  tags     = var.tags
+}
+
