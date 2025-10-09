c1d3836315ac808a299bf611d5d6a530e72938ca
Chai Nadig <chaitanya.nadig@gmail.com>
2025-10-09 23:33:51 +0530

    public repo for website

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -36,7 +36,7 @@ variable "tags" {
 variable "website_image_uri" {
   type        = string
   description = "ECR URI for the website Docker image"
-  default     = "086325458599.dkr.ecr.ap-south-1.amazonaws.com/roommitra/website:latest"
+  default     = "public.ecr.aws/p1s3y6q3/roommitra/website:latest"
 }
 
 variable "api_image_uri" {
