a17b0855efa7cfb41103bc80a8bf26e3c6952159
Chai Nadig <chaitanya.nadig@gmail.com>
2025-10-16 11:03:34 +0530

    test

--- website/src/lib/urls.js
+++ website/src/lib/urls.js
@@ -1,4 +1,5 @@
 export function absoluteUrl(path = '') {
+  console.log(process.env.NEXT_PUBLIC_SITE_URL);
   const base = process.env.NEXT_PUBLIC_SITE_URL || 'https://roommitra.com';
   return new URL(path, base).toString();
 }
