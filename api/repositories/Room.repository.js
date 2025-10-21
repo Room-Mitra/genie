59781697d47aff88d0248a9cc69f075c93b38d02
Chai Nadig <chaitanya.nadig@gmail.com>
2025-10-21 11:08:54 +0530

    linting

--- api/repositories/Room.repository.js
+++ api/repositories/Room.repository.js
@@ -1,15 +1,15 @@
-import { buildHotelEntityItem } from "#common/hotelEntity.helper.js";
-import  DDB  from "#config/DynamoDb.config.js";
-import { ENTITY_TABLE_NAME } from "#Constants/DB.constants.js";
+import { buildHotelEntityItem } from '#common/hotelEntity.helper.js';
+import DDB from '#config/DynamoDb.config.js';
+import { ENTITY_TABLE_NAME } from '#Constants/DB.constants.js';
 
 export const createRoom = async (roomData) => {
-  const roomItem = buildHotelEntityItem(roomData, { includeHotelTypeIndex: true })
+  const roomItem = buildHotelEntityItem(roomData, { includeHotelTypeIndex: true });
   const params = {
     TableName: ENTITY_TABLE_NAME,
     Item: roomItem,
-  }
+  };
 
-  await DDB.put(params).promise(); 
+  await DDB.put(params).promise();
 
   return params.Item;
-} 
+};
