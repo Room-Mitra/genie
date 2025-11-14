0d8ccb5ebd9280a8949df77739f28efbc70cb7dd
Adithya Prabhu <prabhu.adithya@gmail.com>
2025-11-14 21:12:53 +0530

    fixing typo

--- webapp/src/app/ServiceWorkerInit.jsx
+++ webapp/src/app/ServiceWorkerInit.jsx
@@ -5,7 +5,7 @@ export default function ServiceWorkerInit() {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
-        .register("/sw.js")
+        .register("/service-worker.js")
         .then((reg) => console.log("Service Worker registered:", reg))
         .catch((err) => console.error("Service Worker registration failed:", err));
     }
