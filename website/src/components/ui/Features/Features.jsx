90ce93bb675c6da930838c02de9aff88a7bd97de
Chai Nadig <chaitanya.nadig@gmail.com>
2025-11-19 12:24:52 +0530

    savings calculator (#179)

--- website/src/components/ui/Features/Features.jsx
+++ website/src/components/ui/Features/Features.jsx
@@ -8,6 +8,7 @@ import {
   PhoneIcon,
   UsersIcon,
 } from '@heroicons/react/24/outline';
+import GradientWrapper from '../../GradientWrapper';
 
 const Features = () => {
   const featuresList = [
@@ -46,48 +47,53 @@ const Features = () => {
   return (
     <SectionWrapper id="why">
       <div className="custom-screen text-gray-300">
-        <LayoutEffect
-          className="duration-1000 delay-300"
-          isInviewState={{
-            trueState: 'opacity-100',
-            falseState: 'opacity-0 translate-y-6',
-          }}
+        <GradientWrapper
+          className="mt-16 sm:mt-28"
+          wrapperclassname="max-w-3xl h-[250px] top-12 inset-0 sm:h-[300px] lg:h-[300px]"
         >
-          <div className="max-w-xl mx-auto text-center">
-            <h2 className="text-gray-50 text-3xl font-semibold sm:text-4xl">
-              Why Hotels Need This Now
-            </h2>
-            <p className="mt-3 text-md">Hospitality Is Changing. Your Operations Should Too.</p>
-          </div>
-        </LayoutEffect>
-        <LayoutEffect
-          className="duration-1000 delay-500"
-          isInviewState={{
-            trueState: 'opacity-100',
-            falseState: 'opacity-0',
-          }}
-        >
-          <div className="relative mt-12">
-            <ul className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-              {featuresList.map((item, idx) => (
-                <li
-                  key={idx}
-                  className="space-y-3 p-4 rounded-xl border border-gray-800"
-                  style={{
-                    background:
-                      'radial-gradient(157.73% 157.73% at 50% -29.9%, rgba(203, 213, 225, 0.16) 0%, rgba(203, 213, 225, 0) 100%)',
-                  }}
-                >
-                  <div className="w-12 h-12 flex items-center justify-center bg-gray-700 rounded-lg text-gray-50">
-                    {item.icon}
-                  </div>
-                  <h3 className="text-lg text-gray-50 font-semibold">{item.title}</h3>
-                  <p className="text-sm">{item.desc}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </LayoutEffect>
+          <LayoutEffect
+            className="duration-1000 delay-300"
+            isInviewState={{
+              trueState: 'opacity-100',
+              falseState: 'opacity-0 translate-y-6',
+            }}
+          >
+            <div className="max-w-xl mx-auto text-center">
+              <h2 className="text-gray-50 text-3xl font-semibold sm:text-4xl">
+                Why Hotels Need This Now
+              </h2>
+              <p className="mt-3 text-md">Hospitality Is Changing. Your Operations Should Too.</p>
+            </div>
+          </LayoutEffect>
+          <LayoutEffect
+            className="duration-1000 delay-500"
+            isInviewState={{
+              trueState: 'opacity-100',
+              falseState: 'opacity-0',
+            }}
+          >
+            <div className="relative mt-12">
+              <ul className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+                {featuresList.map((item, idx) => (
+                  <li
+                    key={idx}
+                    className="space-y-3 p-4 rounded-xl shadow"
+                    style={{
+                      background:
+                        'radial-gradient(157.73% 157.73% at 50% -29.9%, rgba(22, 16, 50, 0.9) 0%, hsla(213, 27%, 84%, 0.00) 100%)',
+                    }}
+                  >
+                    <div className="w-12 h-12 flex items-center justify-center bg-gray-700 rounded-lg text-gray-50">
+                      {item.icon}
+                    </div>
+                    <h3 className="text-lg text-gray-50 font-semibold">{item.title}</h3>
+                    <p className="text-sm">{item.desc}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </LayoutEffect>
+        </GradientWrapper>
       </div>
     </SectionWrapper>
   );
