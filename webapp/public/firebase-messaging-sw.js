c4b3f6290cd560abefda4f6b1d62ab4a2f416899
Adithya Prabhu <prabhu.adithya@gmail.com>
2025-11-21 11:50:59 +0530

    chore(deps): add firebase web SDK

--- webapp/public/firebase-messaging-sw.js
+++ webapp/public/firebase-messaging-sw.js
@@ -12,6 +12,14 @@ firebase.initializeApp({
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 });
 
+console.log('firebase initialized : ', {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+});
+
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function (payload) {
