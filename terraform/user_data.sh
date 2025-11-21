22360ccb1c5fe63c3e74c6389a31c213980fa3a0
Chai Nadig <chaitanya.nadig@gmail.com>
2025-11-21 10:34:59 +0530

    optimize nginx and nextjs (#185)

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -103,6 +103,12 @@ server {
     proxy_set_header Upgrade $http_upgrade;
     proxy_set_header Connection $connection_upgrade;
     proxy_set_header Host $host;
+
+    # helpful timeouts
+    proxy_connect_timeout 5s;
+    proxy_send_timeout    60s;
+    proxy_read_timeout    60s;
+    send_timeout          60s;
   }
 }
 
@@ -117,7 +123,20 @@ server {
 
   location / {
     proxy_pass http://127.0.0.1:4000;
+
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection $connection_upgrade;
+
     proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+
+    proxy_connect_timeout 5s;
+    proxy_send_timeout    300s;
+    proxy_read_timeout    300s;
+    send_timeout          300s;
   }
 }
 
